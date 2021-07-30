f8591264e0d08111df28693e7bda472bbb8265a1
veronica <veronicaoviedolopez@gmail.com>
2021-07-30 15:16:10 -0700

    data seed

--- back-end/ApplicationDbContext.cs
+++ back-end/ApplicationDbContext.cs
@@ -16,6 +16,34 @@ namespace back_end
             modelBuilder.Entity<MoviesGenres>()
                 .HasKey(x => new { x.MovieId, x.GenreId });
 
+
+
+            modelBuilder.Entity<Genre>().HasData(
+                new Genre { Id = 1, Name = "Action"},
+                new Genre { Id = 2, Name = "Adventure"},
+                new Genre { Id = 3, Name = "Comedy"});
+  
+            modelBuilder.Entity<Actor>().HasData(
+                new Actor { Id = 1, Name = "Sam Worthington", Biography="", Photo=""},
+                new Actor { Id = 2, Name = "Tobey Maguire", Biography="", Photo=""},
+                new Actor { Id = 3, Name = "Kirsten Dunst", Biography="", Photo=""});
+
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie { Id = 1, Title = "Avatar", 
+                Summary="A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.", Poster="https://m.media-amazon.com/images/I/41kTVLeW1CL._AC_SY450_.jpg"},
+                new Movie { Id = 2, Title = "Spider Man", 
+                Summary="A strange black entity from another world bonds with Peter Parker and causes inner turmoil as he contends with new villains, temptations, and revenge.", Poster="https://m.media-amazon.com/images/I/51MssEjdaRL._AC_.jpg"});
+
+
+            modelBuilder.Entity<MoviesGenres>().HasData(
+                new MoviesGenres { MovieId = 1, GenreId = 1},
+                new MoviesGenres { MovieId = 2, GenreId = 1},
+                new MoviesGenres { MovieId = 2, GenreId = 2});
+           
+           modelBuilder.Entity<MoviesActors>().HasData(
+                new MoviesActors { MovieId = 1, ActorId = 1, Character="Jake Sully"},
+                new MoviesActors { MovieId = 2, ActorId = 1, Character="Spider Man"},
+                new MoviesActors { MovieId = 2, ActorId = 3, Character="Mary Jane Watson"});
             base.OnModelCreating(modelBuilder);
         }
 
